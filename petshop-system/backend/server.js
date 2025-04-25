f1392721c3a13e51f4fc684078dcf1a86be44b5f
João Pedro <goncalvesholandajoaopedro@gmail.com>
2025-04-25 19:28:02 -0300

    conserto no serverjs

--- petshop-system/backend/server.js
+++ petshop-system/backend/server.js
@@ -1,3 +1,4 @@
+// --- IMPORTAÇÕES ---
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -7,21 +8,23 @@ const authRoutes = require('./routes/authRoutes');
 const petRoutes = require('./routes/petRoutes');
 const { verifyToken } = require('./middleware/authMiddleware');
 
-app.use('/api/pets', verifyToken, petRoutes);
-
+// --- INICIAR EXPRESS ---
 const app = express();
 
+// --- MIDDLEWARES ---
 app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
+// --- ROTAS ---
 app.use('/api/auth', authRoutes);
 app.use('/api/pets', verifyToken, petRoutes);
 
+// --- CONEXÃO COM BANCO ---
 sequelize.sync().then(() => {
     console.log('Database synced');
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
 }).catch(err => {
-    console.error('Erro na conexão com o banco de dados', err);
+    console.error('Erro de conexão com banco de dados:', err);
 });
