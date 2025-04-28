caa6226accd6272fbcf77687c14ce2c7898751dd
João Pedro <goncalvesholandajoaopedro@gmail.com>
2025-04-28 18:54:26 -0300

    conserto do banco

--- petshop-system/backend/banco_petshop.sql
+++ petshop-system/backend/banco_petshop.sql
@@ -1,24 +1,22 @@
-CREATE DATABASE IF NOT EXISTS petshop;
+-- Criar o banco de dados
+CREATE DATABASE IF NOT EXISTS banco_petshop;
+USE banco_petshop;
 
-USE petshop;
-
+-- Criar tabela de usuários
 CREATE TABLE IF NOT EXISTS Users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  password VARCHAR(255) NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
 );
 
+-- Criar tabela de pets
 CREATE TABLE IF NOT EXISTS Pets (
   id INT AUTO_INCREMENT PRIMARY KEY,
   user_id INT,
-  pet_name VARCHAR(255),
-  breed VARCHAR(255),
-  appointment_date DATETIME,
+  pet_name VARCHAR(255) NOT NULL,
+  breed VARCHAR(255) NOT NULL,
+  appointment_date DATETIME NOT NULL,
   notes TEXT,
   image_path VARCHAR(255),
   FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE
 );
-
-ALTER TABLE Users
-ADD COLUMN createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-ADD COLUMN updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
